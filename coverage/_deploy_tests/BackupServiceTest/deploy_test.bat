26aa1c58dc2e00201c51c7f0fdd338f8df015ea6
Maciej Bajer <157230306+maciejbajer1@users.noreply.github.com>
2025-06-29 14:11:57 +0200

    coverage of tests

---
+++ coverage/_deploy_tests/BackupServiceTest/deploy_test.bat
@@ -0,0 +1,6 @@
+@echo off
+set QTDIR=C:\Qt6.8.0\Qt\6.8.0\msvc2022_64
+set EXE=C:\Projects\project\out\build\debug\BackupServiceTest.exe
+
+"%QTDIR%\bin\windeployqt.exe" %EXE%
+pause
---
+++ coverage/_deploy_tests/TipManagerTest/run_coverage.bat
@@ -0,0 +1,8 @@
+@echo off
+set COV="C:\Program Files\OpenCppCoverage\OpenCppCoverage.exe"
+set SRC="C:\Projects\project"
+set BIN="C:\Projects\project\out\build\debug\TipManagerTest.exe"
+set OUT="C:\Projects\project\coverage"
+
+%COV% --sources %SRC% --export_type html:%OUT% -- %BIN%
+start "" %OUT%\index.html
